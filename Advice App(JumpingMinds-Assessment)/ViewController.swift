1a16aef4198986d127b46e60903c025a4f7684ec
Dishant Nagpal <dishantnagpal10@gmail.com>
2022-06-25 15:40:40 +0530

    Initial Commit

---
+++ Advice App(JumpingMinds-Assessment)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Advice App(JumpingMinds-Assessment)
+//
+//  Created by Dishant Nagpal on 25/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
